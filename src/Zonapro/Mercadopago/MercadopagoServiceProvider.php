f70c0dbbd856eb2a7c585dc2058110d8125d48fd
Jorge Saud <jorgelsaud@gmail.com>
2014-09-21 14:18:24 -0430

    first commit

---
+++ src/Zonapro/Mercadopago/MercadopagoServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Zonapro\Mercadopago;
+
+use Illuminate\Support\ServiceProvider;
+
+class MercadopagoServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('zonapro/mercadopago');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array('mercadopago');
+	}
+
+}
